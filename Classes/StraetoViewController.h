12d8c23380f7d8275ef4e55504c80e0188640fa0
addi <addininja@gmail.com>
2012-01-05 06:47:25 +0000

    first commit, just a mapview of Reykjavik

---
+++ Classes/StraetoViewController.h
@@ -0,0 +1,18 @@
+//
+//  StraetoViewController.h
+//  Straeto
+//
+//  Created by Árni Jónsson on 5.1.2012.
+//  Copyright 2012 ProNasty. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <MapKit/MapKit.h>
+
+@interface StraetoViewController : UIViewController <MKMapViewDelegate> {
+	MKMapView *_mapView;
+}
+
+@property (nonatomic, retain) IBOutlet MKMapView *mapView;
+
+@end
---
+++ Classes/StraetoViewController.m
@@ -0,0 +1,86 @@
+//
+//  StraetoViewController.m
+//  Straeto
+//
+//  Created by Árni Jónsson on 5.1.2012.
+//  Copyright 2012 ProNasty. All rights reserved.
+//
+
+#import "StraetoViewController.h"
+
+@implementation StraetoViewController
+@synthesize mapView = _mapView;
+
+
+- (void)dealloc
+{
+    [_mapView release];
+    [super dealloc];
+}
+
+- (void)viewWillAppear:(BOOL)animated {  
+    // 1
+    CLLocationCoordinate2D zoomLocation;
+    zoomLocation.latitude = 64.133004;
+    zoomLocation.longitude = -21.89764;
+	
+    // 2
+    MKCoordinateRegion viewRegion = MKCoordinateRegionMakeWithDistance(zoomLocation, 2000.0, 2000.0);
+    // 3
+    MKCoordinateRegion adjustedRegion = [_mapView regionThatFits:viewRegion];                
+    // 4
+    [_mapView setRegion:adjustedRegion animated:YES];        
+}
+
+
+/*
+// The designated initializer. Override to perform setup that is required before the view is loaded.
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+*/
+
+/*
+// Implement loadView to create a view hierarchy programmatically, without using a nib.
+- (void)loadView {
+}
+*/
+
+
+/*
+// Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+*/
+
+
+/*
+// Override to allow orientations other than the default portrait orientation.
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
+    // Return YES for supported orientations
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+*/
+
+- (void)didReceiveMemoryWarning {
+	// Releases the view if it doesn't have a superview.
+    [super didReceiveMemoryWarning];
+	
+	// Release any cached data, images, etc that aren't in use.
+}
+
+- (void)viewDidUnload {
+	// Release any retained subviews of the main view.
+	// e.g. self.myOutlet = nil;
+	
+	[self setMapView:nil];
+    [super viewDidUnload];
+
+}
+
+@end
